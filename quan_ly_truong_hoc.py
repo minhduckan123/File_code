7e023d743b8148cced32e30a5d4040cb5b50e480
ducdut <93233277+minhduckan123@users.noreply.github.com>
2022-03-03 17:29:35 +0700

    Update quan_ly_truong_hoc.py

--- quan_ly_truong_hoc.py
+++ quan_ly_truong_hoc.py
@@ -10,16 +10,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://minhduc@localhost/minhduc"
 db = SQLAlchemy(app)
 
 class Classroom(db.Model):
-    class_room_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     capacity = db.Column(db.Integer, nullable=False)
-    classes = db.relationship("Class", backref="classroom", lazy=True)
+    classes = db.relationship("Class")
 
 
 class Subject(db.Model):
-    subject_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
-    classes = db.relationship("Class", backref="subject", lazy=True)
+    classes = db.relationship("Class")
 
     def __init__(self,name,capacity):
         self.name = name
@@ -30,49 +30,44 @@ class Subject(db.Model):
 
 
 class Teacher(db.Model):
-    teacher_id = db.Column(db.Integer, primary_key=True)
-    fisrt_name = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     subject_taught = db.Column(db.String(100), nullable=False)
-    classes = db.relationship("Class", backref="teacher", lazy=True)
+    classes = db.relationship("Class")
 
-    def __init__(self,fisrt_name,last_name,subject_taught):
-        self.fisrt_name = fisrt_name
+    def __init__(self,first_name,last_name,subject_taught):
+        self.first_name = first_name
         self.last_name = last_name
         self.subject_taught = subject_taught
 
     def __repr__(self):
-        return f"Teacher('{self.fisrt_name}', '{self.last_name}, '{self.subject_taught}')"
+        return f"Teacher('{self.first_name}', '{self.last_name}, '{self.subject_taught}')"
 
+
+classList = db.Table("classList",
+    db.Column("student_id", db.Integer, db.ForeignKey("student.id"), primary_key=True),
+    db.Column("class_id", db.Integer, db.ForeignKey("class.id"), primary_key=True)
+)
+    
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_room_id = db.Column(db.Integer, db.ForeignKey("classroom.id"), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"), nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey("teacher.id"), nullable=False)
+    classList = db.relationship("Student", secondary=classList)
 
 class Student(db.Model):
-    student_id = db.Column(db.Integer, primary_key=True)
-    fisrt_name = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     date_of_birth = db.Column(db.DateTime, nullable=False)
-    classList = db.relationship("ClassList", backref="student", lazy=True)
 
-    def __init__(self,fisrt_name,last_name,date_of_birth):
-        self.fisrt_name = fisrt_name
+    def __init__(self,first_name,last_name,date_of_birth):
+        self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
 
     def __repr__(self):
-        return f"Student('{self.fisrt_name}', '{self.last_name}', '{self.date_of_birth}')"
+        return f"Student('{self.first_name}', '{self.last_name}', '{self.date_of_birth}')"
 
-class ClassList(db.Model):
-    class_list_id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey("Student.student_id"), nullable=False)
-    classes = db.relationship("Class")
-
-
-
-class Class(db.Model):
-    class_id = db.Column(db.Integer, primary_key=True)
-    class_room_id = db.Column(db.Integer, db.ForeignKey("Classroom.class_room_id"), nullable=False)
-    subject_id = db.Column(db.Integer, db.ForeignKey("Subject.subject_id"), nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey("Teacher.teacher_id"), nullable=False)
-    class_list_id = db.Column(db.Integer, db.ForeignKey("ClassList.class_list_id"), nullable=False)
-
-
-db.create_all()
