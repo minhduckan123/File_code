9ddd6e3f787a332ffa9dafb8c391959759757506
ducdut <93233277+minhduckan123@users.noreply.github.com>
2022-03-04 23:56:58 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,16 +1,32 @@
-from flask import Flask, render_template
-from quan_ly_truong_hoc import Student, app
+from flask import Flask, render_template, request, url_for, redirect, flash
+from quan_ly_truong_hoc import Student, app, db
 
 
 @app.route("/")
 @app.route("/trang_chu")
 def trang_chu():
-    return render_template("trang_chu.html")
+    return render_template("trang_chu.html", title="Trang chủ")
+
 
 @app.route("/hoc_sinh")
 def hoc_sinh():
     students = Student.query.all()
-    return render_template("hoc_sinh.html", students=students)
+    return render_template("hoc_sinh.html", students=students, title="Học sinh")
+
+@app.route("/insert", methods=["POST"])
+def insert():
+    if request.method == "POST":
+        firstname = request.form["first_name"]
+        lastname = request.form["last_name"]
+        date_of_birth = request.form["date_of_birth"]
+
+        student_insert = Student(firstname, lastname, date_of_birth)
+        db.session.add(student_insert)
+        db.session.commit()
+
+        flash("Success")
+
+        return redirect(url_for("hoc_sinh"))
 
 if __name__ == "__main__":
     app.run(debug=True)
