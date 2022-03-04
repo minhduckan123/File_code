3fdea38c0d78e012f6f7e0992a47bd22fe95dba8
ducdut <93233277+minhduckan123@users.noreply.github.com>
2022-03-04 23:55:30 +0700

    Update quan_ly_truong_hoc.py

--- quan_ly_truong_hoc.py
+++ quan_ly_truong_hoc.py
@@ -9,24 +9,33 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://minhduc@localhost/minhduc"
 
 db = SQLAlchemy(app)
 
+
 class Classroom(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     capacity = db.Column(db.Integer, nullable=False)
-    classes = db.relationship("Class")
+    classes = db.relationship("Class", backref="classroom")
+
+    @property
+    def uid(self):
+        return f"PH{self.id}"
+
+    def __init__(self, capacity):
+        self.capacity = capacity
 
 
 class Subject(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
-    classes = db.relationship("Class")
+    classes = db.relationship("Class", backref="subject")
 
-    def __init__(self,name,capacity):
+    @property
+    def uid(self):
+        return f"MH{self.id}"
+
+    def __init__(self, name, capacity):
         self.name = name
         self.capacity = capacity
-
-    def __repr__(self):
-        return f"Subject('{self.name}', '{self.capacity}')"
 
 
 class Teacher(db.Model):
@@ -34,40 +43,52 @@ class Teacher(db.Model):
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     subject_taught = db.Column(db.String(100), nullable=False)
-    classes = db.relationship("Class")
+    classes = db.relationship("Class", backref="teacher")
 
-    def __init__(self,first_name,last_name,subject_taught):
+    @property
+    def uid(self):
+        return f"GV{self.id}"
+
+    def __init__(self, first_name, last_name, subject_taught):
         self.first_name = first_name
         self.last_name = last_name
         self.subject_taught = subject_taught
 
-    def __repr__(self):
-        return f"Teacher('{self.first_name}', '{self.last_name}, '{self.subject_taught}')"
-
-
-classList = db.Table("classList",
-    db.Column("student_id", db.Integer, db.ForeignKey("student.id"), primary_key=True),
-    db.Column("class_id", db.Integer, db.ForeignKey("class.id"), primary_key=True)
-)
-    
-class Class(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    class_room_id = db.Column(db.Integer, db.ForeignKey("classroom.id"), nullable=False)
-    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"), nullable=False)
-    teacher_id = db.Column(db.Integer, db.ForeignKey("teacher.id"), nullable=False)
-    classList = db.relationship("Student", secondary=classList)
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     date_of_birth = db.Column(db.DateTime, nullable=False)
+    classlists = db.relationship("ClassList", backref="student")
 
-    def __init__(self,first_name,last_name,date_of_birth):
+    @property
+    def uid(self):
+        return f"HS{self.id}"
+
+    def __init__(self, first_name, last_name, date_of_birth="0000-00-00 00.00.00"):
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
 
-    def __repr__(self):
-        return f"Student('{self.first_name}', '{self.last_name}', '{self.date_of_birth}')"
 
+class ClassList(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey("student.id"))
+    classes = db.relationship("Class", backref="class_list")
+
+
+class Class(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    class_room_id = db.Column(db.Integer, db.ForeignKey("classroom.id"))
+    subject_id = db.Column(db.Integer, db.ForeignKey("subject.id"))
+    teacher_id = db.Column(db.Integer, db.ForeignKey("teacher.id"))
+    class_list_id = db.Column(db.Integer, db.ForeignKey("class_list.id"))
+
+    @property
+    def uid(self):
+        return f"LH{self.id}"
+
+
+# db.drop_all()
+# db.create_all()
