a2b4170af2cbd18d374e8a93c06d83f705e21a8b
ducdut <93233277+minhduckan123@users.noreply.github.com>
2022-03-07 12:50:31 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,19 +1,20 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
-from quan_ly_truong_hoc import Student, Classroom, ClassList, Subject, Teacher, app, db
+from re import sub
+from flask import render_template, request, url_for, redirect, flash
+from database import *
 
 
 @app.route("/")
-@app.route("/trang_chu")
+@app.route("/home")
 def trang_chu():
-    return render_template("trang_chu.html", title="Home")
+    return render_template("home.html", title="Home")
 
 
-@app.route("/lop_hoc")
+@app.route("/classroom")
 def lop_hoc():
     classrooms = Classroom.query.all()
-    return render_template("lop_hoc.html", classrooms=classrooms, title="Classroom")
+    return render_template("classroom.html", classrooms=classrooms, title="Classroom")
 
-@app.route("/lop_hoc/insert", methods=["POST"])
+@app.route("/classroom/insert", methods=["POST"])
 def insert_lop_hoc():
     if request.method == "POST":
         capacity = request.form["capacity"]
@@ -26,7 +27,7 @@ def insert_lop_hoc():
 
         return redirect(url_for("lop_hoc"))
 
-@app.route("/lop_hoc/update", methods=["GET","POST"])
+@app.route("/classroom/update", methods=["GET","POST"])
 def update_lop_hoc():
     
     if request.method == "POST":
@@ -40,7 +41,7 @@ def update_lop_hoc():
 
         return redirect(url_for("lop_hoc"))
     
-@app.route("/lop_hoc/delete/<id>/", methods=["GET", "POST"])
+@app.route("/classroom/delete/<id>/", methods=["GET", "POST"])
 def delete_lop_hoc(id):
     del_classroom = Classroom.query.get(id)
     db.session.delete(del_classroom)
@@ -51,59 +52,76 @@ def delete_lop_hoc(id):
     return redirect(url_for("lop_hoc"))
 
 
-@app.route("/hoc_sinh")
+@app.route("/student")
 def hoc_sinh():
     students = Student.query.all()
-    return render_template("hoc_sinh.html", students=students, title="Student")
+    classlists = ClassList
+    
+    return render_template("student.html", students=students, classlists=classlists)
 
-@app.route("/hoc_sinh/insert", methods=["POST"])
+@app.route("/student/insert", methods=["POST"])
 def insert_hoc_sinh():
     if request.method == "POST":
-        firstname = request.form["first_name"]
-        lastname = request.form["last_name"]
+        classroom_list_id = Classroom.query.with_entities(Classroom.id).all()
+        a = [i[0] for i in classroom_list_id]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
         date_of_birth = request.form["date_of_birth"]
+        classroom_id = request.form["classroom_id"]
+        if int(classroom_id) in a:
+            classlist = ClassList(student_id=classroom_id)
+            db.session.add(classlist)
+        else:
+            raise
 
-        student_insert = Student(firstname, lastname, date_of_birth)
-        db.session.add(student_insert)
+        subject_insert = Student(first_name, last_name, date_of_birth)
+        db.session.add(subject_insert)
         db.session.commit()
 
         flash("Successed!")
 
         return redirect(url_for("hoc_sinh"))
 
-@app.route("/hoc_sinh/update", methods=["GET","POST"])
+@app.route("/student/update", methods=["POST"])
 def update_hoc_sinh():
-    
     if request.method == "POST":
-        edit_student = Student.query.get(request.form.get("id"))
-
+        edit_student= Student.query.get(request.form.get("id"))
+        edit_class_list = ClassList.query.get(request.form.get("id"))
+        classroom_list_id = Classroom.query.with_entities(Classroom.id).all()
+        a = [i[0] for i in classroom_list_id]
         edit_student.first_name = request.form["first_name"]
         edit_student.last_name = request.form["last_name"]
         edit_student.date_of_birth = request.form["date_of_birth"]
-
+        classroom_id = request.form["classroom_id"]
+        if int(classroom_id) in a:
+            edit_class_list.student_id = request.form["classroom_id"]
+        else:
+            raise
         db.session.commit()
     
         flash("Successed!")
 
         return redirect(url_for("hoc_sinh"))
     
-@app.route("/hoc_sinh/delete/<id>/", methods=["GET", "POST"])
+@app.route("/student/delete/<id>/", methods=["GET", "POST"])
 def delete_hoc_sinh(id):
     del_student = Student.query.get(id)
+    del_class_list = ClassList.query.get(id)
+    
     db.session.delete(del_student)
+    db.session.delete(del_class_list)
     db.session.commit()
 
     flash("Deleted!")
 
     return redirect(url_for("hoc_sinh"))
-
-
-@app.route("/mon_hoc")
+    
+@app.route("/subject")
 def mon_hoc():
     subjects = Subject.query.all()
-    return render_template("mon_hoc.html", subjects=subjects, title="Subject")
+    return render_template("subject.html", subjects=subjects, title="Subject")
 
-@app.route("/mon_hoc/insert", methods=["POST"])
+@app.route("/subject/insert", methods=["POST"])
 def insert_mon_hoc():
     if request.method == "POST":
         name = request.form["name"]
@@ -117,7 +135,7 @@ def insert_mon_hoc():
 
         return redirect(url_for("mon_hoc"))
 
-@app.route("/mon_hoc/update", methods=["GET","POST"])
+@app.route("/subject/update", methods=["GET","POST"])
 def update_mon_hoc():
     
     if request.method == "POST":
@@ -132,7 +150,7 @@ def update_mon_hoc():
 
         return redirect(url_for("mon_hoc"))
     
-@app.route("/mon_hoc/delete/<id>/", methods=["GET", "POST"])
+@app.route("/subject/delete/<id>/", methods=["GET", "POST"])
 def delete_mon_hoc(id):
     del_subject = Subject.query.get(id)
     db.session.delete(del_subject)
@@ -141,21 +159,19 @@ def delete_mon_hoc(id):
     flash("Deleted!")
 
     return redirect(url_for("mon_hoc"))
-    
 
-@app.route("/giao_vien")
+@app.route("/teacher")
 def giao_vien():
     teachers = Teacher.query.all()
-    return render_template("giao_vien.html", teachers=teachers, title="Teacher")
+    return render_template("teacher.html", teachers=teachers, title="Teacher")
 
-@app.route("/giao_vien/insert", methods=["POST"])
+@app.route("/teacher/insert", methods=["POST"])
 def insert_giao_vien():
     if request.method == "POST":
-        firstname = request.form["first_name"]
-        lastname = request.form["last_name"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
         subject_taught = request.form["subject_taught"]
-
-        teacher_insert = Teacher(firstname, lastname, subject_taught)
+        teacher_insert = Teacher(first_name, last_name, subject_taught)
         db.session.add(teacher_insert)
         db.session.commit()
 
@@ -163,7 +179,7 @@ def insert_giao_vien():
 
         return redirect(url_for("giao_vien"))
 
-@app.route("/giao_vien/update", methods=["GET","POST"])
+@app.route("/teacher/update", methods=["GET","POST"])
 def update_giao_vien():
     
     if request.method == "POST":
@@ -179,7 +195,7 @@ def update_giao_vien():
 
         return redirect(url_for("giao_vien"))
     
-@app.route("/giao_vien/delete/<id>/", methods=["GET", "POST"])
+@app.route("/teacher/delete/<id>/", methods=["GET", "POST"])
 def delete_giao_vien(id):
     del_teacher = Teacher.query.get(id)
     db.session.delete(del_teacher)
@@ -189,5 +205,59 @@ def delete_giao_vien(id):
 
     return redirect(url_for("giao_vien"))
     
+@app.route("/school")
+def truong_hoc():
+    classes = Class.query.all()
+    classroom_count = Classroom.query.count()
+    student_count = Student.query.count()
+    subject_count = Subject.query.count()
+    teacher_count = Teacher.query.count()
+    classrooms = Classroom
+    return render_template("school.html", classes=classes, title="School",
+                           classroom_count=classroom_count, student_count=student_count,
+                           subject_count=subject_count, teacher_count=teacher_count,
+                           classrooms=classrooms)
+
+@app.route("/school/insert", methods=["POST"])
+def insert_truong_hoc():
+    if request.method == "POST":
+        classroom_id = request.form["class_list_id"]
+        subject_id = request.form["subject_id"]
+        teacher_id = request.form["teacher_id"]
+
+        class_insert = Class(classroom_id, subject_id, teacher_id,classroom_id)
+        db.session.add(class_insert)
+        db.session.commit()
+
+        flash("Successed!")
+
+        return redirect(url_for("truong_hoc"))
+
+@app.route("/school/update", methods=["GET","POST"])
+def update_truong_hoc():
+    
+    if request.method == "POST":
+        edit_class = Class.query.get(request.form.get("id"))
+
+        edit_class.class_list_id = int(request.form["class_list_id"])
+        edit_class.subject_id = int(request.form["subject_id"])
+        edit_class.teacher_id = int(request.form["teacher_id"])
+
+        db.session.commit()
+    
+        flash("Successed!")
+
+        return redirect(url_for("truong_hoc"))
+    
+@app.route("/school/delete/<id>/", methods=["GET", "POST"])
+def delete_truong_hoc(id):
+    del_class = Class.query.get(id)
+    db.session.delete(del_class)
+    db.session.commit()
+
+    flash("Deleted!")
+
+    return redirect(url_for("truong_hoc"))
+
 if __name__ == "__main__":
     app.run(debug=True)
